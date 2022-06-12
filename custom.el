a76bc63c100729952b29a66d1c36c9ec1323a5bb
ier <ivan.erakhtin@gmail.com>
2022-06-12 20:50:46 +0300

    Add csharp support

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((cider-clojure-cli-aliases . "-A:test:build")))
  '(package-selected-packages '(cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
