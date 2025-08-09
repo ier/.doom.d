71137007e5c00933dc5cea2e3642c2bfa39a3ac4
ier <ivan.erakhtin@gmail.com>
2025-08-09 15:43:34 +0300

    The tools fine tuning

--- custom.el
+++ custom.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values '((cider-clojure-cli-aliases . "-A:test:build")))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages
-   '(doom-modeline flycheck-clj-kondo lsp-mode cider)))
+ '(package-selected-packages '(cider doom-modeline eglot flycheck-clj-kondo lsp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
